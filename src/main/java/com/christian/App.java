0e150093706c872fb04df5aed0f30b42ba9fd28f
Christian Loyola <dieguito_502@hotmail.com>
2026-09-01 01:28:20 -0500

    Inicializar proyecto Java con Maven

---
+++ src/main/java/com/christian/App.java
@@ -0,0 +1,12 @@
+package com.christian;
+
+public class App {
+
+    public static void main(String[] args) {
+        String mensaje = "Java + Maven funcionando correctamente";
+        int numero = 25;
+
+        System.out.println(mensaje);
+        System.out.println("Número: " + numero);
+    }
+}
